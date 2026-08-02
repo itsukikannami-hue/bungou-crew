8e187b83ab7845d38181c64681c96da12bb25a65
itsuki <itsuki.kannami@gmail.com>
2026-08-02 22:56:49 +0900

    Add X login button

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,10 +1,6 @@
-console.log("URL =", process.env.NEXT_PUBLIC_SUPABASE_URL)
-console.log("KEY =", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+import { createBrowserClient } from "@supabase/ssr"
 
-import { createClient } from "@supabase/supabase-js"
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ""
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
-
-// ここで export する
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+)
